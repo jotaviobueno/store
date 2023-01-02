0b73258df4b3d13dc9b679e75d2900b624a93938
unknown <joseotaviocbueno@gmail.com>
2023-01-02 14:44:12 -0300

    adicionado as rotas

--- src/application.js
+++ src/application.js
@@ -1,9 +1,12 @@
 import cors from "cors";
 
+import {indexRouter} from "./routes/index.js";
+
 export default function Application (app, express) {
 	app.use(cors());
 	app.use(express());
 	app.use(express.urlencoded({ extended: true }));
 	app.use(express.json({limit: "50mb"}));
+	app.use(indexRouter);
 	
 }
