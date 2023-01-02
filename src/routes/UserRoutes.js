d9cdd17978d99a0dad541f9076bdf66fddf9b9ef
unknown <joseotaviocbueno@gmail.com>
2023-01-02 14:46:44 -0300

    adicionada a importação do controller

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -2,6 +2,6 @@ import {Router} from "express";
 
 export const userRoutes = Router();
 
-import UserController from "../modules/user/UserController";
+import UserController from "../modules/user/UserController.js";
 
 userRoutes.post("/", UserController.create);
