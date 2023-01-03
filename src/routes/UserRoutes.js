20d8aa51aad7b2b070a2bfcf0c8205f908ec21e3
unknown <joseotaviocbueno@gmail.com>
2023-01-03 17:44:26 -0300

    criado a rota

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -4,4 +4,9 @@ export const userRoutes = Router();
 
 import UserController from "../modules/user/UserController.js";
 
+// Middlawares
+import ValidateSession from "../Middleware/ValidateSession.js";
+import ValidateUser from "../Middleware/ValidateUser.js";
+
 userRoutes.post("/", UserController.create);
+userRoutes.get("/", ValidateSession, ValidateUser, UserController.showProfile);
