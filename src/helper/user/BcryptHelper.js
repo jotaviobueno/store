43bebdd9008e581cad58dc77a524213a6abc4c2d
unknown <joseotaviocbueno@gmail.com>
2023-01-03 01:13:43 -0300

    adicionado o compare password

--- src/helper/user/BcryptHelper.js
+++ src/helper/user/BcryptHelper.js
@@ -4,6 +4,10 @@ class BcryptHelper {
 	async generateHash(password, salt) {
 		return await hash(password, salt ?? 10);
 	}
+
+	async comparePassword(password, hash) {
+		return await compare(password, hash);
+	}
 }
 
 export default new BcryptHelper;
