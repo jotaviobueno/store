9653f5d9f3ed76138c8eaf70402a5658e0841536
unknown <joseotaviocbueno@gmail.com>
2023-01-03 18:53:48 -0300

    rota de trocar o username criado

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,6 +3,7 @@ import {Router} from "express";
 export const userRoutes = Router();
 
 import UserController from "../modules/user/UserController.js";
+import UpdateUserController from "../modules/User/UpdateUserController.js";
 
 // Middlawares
 import ValidateSession from "../Middleware/ValidateSession.js";
@@ -11,3 +12,5 @@ import ValidateUser from "../Middleware/ValidateUser.js";
 userRoutes.post("/", UserController.create);
 userRoutes.get("/", ValidateSession, ValidateUser, UserController.showProfile);
 userRoutes.get("/:username", ValidateSession, ValidateUser, UserController.showOutherProfile);
+
+userRoutes.patch("/update/username", ValidateSession, ValidateUser, UpdateUserController.updateUsername);
