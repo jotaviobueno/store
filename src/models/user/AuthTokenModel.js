18b9790841b528d1ac29671931884d0949140f1b
unknown <joseotaviocbueno@gmail.com>
2023-01-10 12:52:02 -0300

    alteração no model de autenticação

--- src/models/user/AuthTokenModel.js
+++ src/models/user/AuthTokenModel.js
@@ -6,7 +6,6 @@ const AuthTokenModel = new Schema({
 
 	token: { type: String, required: true, },
 	status: { type: String, required: true, default: "generated" },
-	email: { type: String, required: true, },
 	user_id: { type: mongoose.Types.ObjectId, required: true, ref: "Users" },
 	token_generated_for: { type: String, required: true },
 	expires_at: { type: Date, required: true, default: new Date().setHours(new Date().getHours() + 6) },
