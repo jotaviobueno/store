a2264aa911312a9e545316b9d2b0b2e1ac9ecc43
unknown <joseotaviocbueno@gmail.com>
2023-01-10 12:48:00 -0300

    criado a rota de geração de token para exclusão de conta

--- src/routes/AuthRoutes.js
+++ src/routes/AuthRoutes.js
@@ -4,4 +4,9 @@ export const authRoutes = Router();
 
 import AuthTokenController from "../modules/User/AuthTokenController.js";
 
+// Middlawares
+import ValidateSession from "../Middleware/ValidateSession.js";
+import ValidateUser from "../Middleware/ValidateUser.js";
+
 authRoutes.post("/generate-token/change-password", AuthTokenController.generateTokenToChangePassword);
+authRoutes.post("/generate-token/delete-account", ValidateSession, ValidateUser, AuthTokenController.generateTokenToDeleteAccount);
