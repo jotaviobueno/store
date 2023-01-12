cb03259c72e2ec80f659a0c067be25d5dcedd602
unknown <joseotaviocbueno@gmail.com>
2023-01-12 13:41:49 -0300

    transformado a função em uma class

--- src/DTO/Response/Product/ProductResponseDTO.js
+++ src/DTO/Response/Product/ProductResponseDTO.js
@@ -1,16 +1,29 @@
-export default function ProductResponseDTO(product) {
-	return {
-		name: product.name,
-		description: product.description,
-		product_id: product.product_id,
-		price: product.price,
-		discount: `%${product.discount}`,
-		stock: product.stock,
-		category: product.category,
-		product_images: product.product_images, 
-		is_active: product.is_active,
-		created_at: product.created_at,
-		updated_at: product.updated_at,
-		reviews: product.reviews,
-	};
+
+
+
+
+class ProductResponseDTO {
+	
+	treatOnlyOneProduct(product) {
+		return {
+			name: product.name,
+			description: product.description,
+			product_id: product.product_id,
+			price: product.price,
+			discount: `%${product.discount}`,
+			stock: product.stock,
+			category: product.category,
+			product_images: product.product_images, 
+			is_active: product.is_active,
+			created_at: product.created_at,
+			updated_at: product.updated_at,
+			reviews: product.reviews,
+		};
+	}
+
+	treatManyProducts(products) {
+		return products.map(product => this.treatOnlyOneProduct(product));
+	}
 }
+
+export default new ProductResponseDTO;
