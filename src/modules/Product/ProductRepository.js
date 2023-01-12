6e4fd522a2c201f9b9263cb272df58678cb33203
unknown <joseotaviocbueno@gmail.com>
2023-01-11 22:44:54 -0300

    criado o findByProductId é o update

--- src/modules/Product/ProductRepository.js
+++ src/modules/Product/ProductRepository.js
@@ -1,5 +1,7 @@
 import ProductModel from "../../models/Product/ProductModel.js";
 
+import { nanoid } from "nanoid";
+
 class ProductRepository {
 
 	_productModel;
@@ -12,6 +14,7 @@ class ProductRepository {
 		return await this._productModel.create({
 			name: product.name,
 			description: product.description,
+			product_id: nanoid(),
 			user_id: user_id,
 			price: Number(product.price),
 			stock: Number(product.stock),
@@ -20,6 +23,15 @@ class ProductRepository {
 		});
 	}
 
+	async update(_id, fildToUpdate, value) {
+		return await this._productModel.updateOne({_id, deleted_at: null}, {
+			[fildToUpdate]: value, updated_at: new Date()
+		});
+	}
+
+	async findProductById(product_id) {
+		return await this._productModel.findOne({ product_id, deleted_at: null });
+	}
 }
 
 export default ProductRepository;
