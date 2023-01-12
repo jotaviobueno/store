c6ab1c4006655266279d9ffb5fd2c87de072dee1
unknown <joseotaviocbueno@gmail.com>
2023-01-12 14:25:32 -0300

    adicionado a importação das rotas

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -12,8 +12,10 @@ import ValidateProduct from "../Middleware/ValidateProduct.js";
 import ValidateProductOwner from "../Middleware/ValidateProductOwner.js";
 
 productRoutes.post("/", ValidateSession, ValidateUser, ProductController.createProduct);
-productRoutes.get("/", ValidateSession, ValidateUser, ProductController.showProdutcs);
+productRoutes.get("/many", ValidateSession, ValidateUser, ProductController.showManyProduct);
+productRoutes.get("/", ValidateSession, ValidateUser, ValidateProduct, ProductController.showProduct);
 
 productRoutes.patch("/update/name", ValidateSession, ValidateUser, ValidateProduct, ValidateProductOwner, UpdateProductController.updateProductName);
 productRoutes.patch("/update/description", ValidateSession, ValidateUser, ValidateProduct, ValidateProductOwner, UpdateProductController.updateProductDescription);
 productRoutes.patch("/update/price", ValidateSession, ValidateUser, ValidateProduct, ValidateProductOwner, UpdateProductController.updateProductPrice);
+productRoutes.patch("/update/stock", ValidateSession, ValidateUser, ValidateProduct, ValidateProductOwner, UpdateProductController.updateProductStock);
