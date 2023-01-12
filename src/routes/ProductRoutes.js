1133635ed3559c5230cfb28fdb60f860461f6e9b
unknown <joseotaviocbueno@gmail.com>
2023-01-11 22:45:18 -0300

    criado a rota de update Product name

--- src/routes/ProductRoutes.js
+++ src/routes/ProductRoutes.js
@@ -3,9 +3,13 @@ import {Router} from "express";
 export const productRoutes = Router();
 
 import ProductController from "../modules/Product/ProductController.js";
+import UpdateProductController from "../modules/Product/UpdateProductController.js";
 
 // Middlawares
 import ValidateSession from "../Middleware/ValidateSession.js";
 import ValidateUser from "../Middleware/ValidateUser.js";
+import ValidateProduct from "../Middleware/ValidateProduct.js";
+import ValidateProductOwner from "../Middleware/ValidateProductOwner.js";
 
 productRoutes.post("/", ValidateSession, ValidateUser, ProductController.createProduct);
+productRoutes.patch("/update/name", ValidateSession, ValidateUser, ValidateProduct, ValidateProductOwner, UpdateProductController.updateProductName);
